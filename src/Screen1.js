8a8747388f550bdbd3cf4622ca202c9dc78da9e4
arslanali19470 <arslanali19470@gmail.com>
2024-06-11 06:45:03 -0700

    GoRide Toolkit C1

---
+++ src/Screen1.js
@@ -0,0 +1,45 @@
+// Screen1.js
+import {Button, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {PushArray, ChangeName} from './dataSlice';
+
+const Screen1 = () => {
+  const dispatch = useDispatch();
+  const FirstArray = useSelector(state => state.data.array1);
+  const OwnName = useSelector(state => state.data.Myname);
+  const [inputValue, setInputValue] = useState('');
+  const [NameValue, setNameValue] = useState('');
+
+  const handleAdd = () => {
+    dispatch(PushArray(inputValue));
+    setInputValue('');
+  };
+
+  const handleChangeName = () => {
+    dispatch(ChangeName(NameValue));
+  };
+
+  return (
+    <View>
+      <Text>{OwnName}</Text>
+      <TextInput
+        style={{borderWidth: 2, borderColor: 'black', marginBottom: 10}}
+        value={NameValue}
+        onChangeText={setNameValue}
+      />
+      <Button title="Change Name" onPress={handleChangeName} />
+      <TextInput
+        style={{borderWidth: 2, borderColor: 'black', marginBottom: 10}}
+        value={inputValue}
+        onChangeText={setInputValue}
+      />
+      <Button title="Add" onPress={handleAdd} />
+      {FirstArray.map((item, ind) => (
+        <Text key={ind}>{item}</Text>
+      ))}
+    </View>
+  );
+};
+
+export default Screen1;
